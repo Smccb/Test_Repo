dd505d9b30fc815a57b1eceb1f5f084f145348fb
Smccb <sarahmmccb@gmail.com>
2022-12-04 17:40:11 +0000

    Update testClass.java

--- testProject/src/testProject/testClass.java
+++ testProject/src/testProject/testClass.java
@@ -5,5 +5,9 @@ public class testClass {
 		System.out.println("Hello World!!");
 		System.out.println("This is the other branch and has differences");
 		System.out.println("This is a tester branch update");
+		
+		
+		System.out.println("more updates to project");
+		//comments for testing committing to and pulling from project.....
 	}
 }
