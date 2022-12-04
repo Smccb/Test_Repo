5718d2b6e1671c8afecb643141652aa1e1575acd
Smccb <sarahmmccb@gmail.com>
2022-12-04 17:29:21 +0000

    Squashed commit of the following:
    commit 1079ab3d61620853715a5e48c94e1bd9ca655f6f
    Author: Smccb <sarahmmccb@gmail.com>
    Date:   Sun Dec 4 17:24:12 2022 +0000
    
        commit
    
    commit 61788ff76e1cf9f79bb405203f270b14a10628bc
    Author: Smccb <sarahmmccb@gmail.com>
    Date:   Sun Dec 4 16:25:26 2022 +0000
    
        push to new branch
    
    commit de212238d0dce9ed75794c7107a5c70bd346a33d
    Author: Smccb <sarahmmccb@gmail.com>
    Date:   Sun Dec 4 16:29:52 2022 +0000
    
        commit to other branch

--- testProject/src/testProject/testClass.java
+++ testProject/src/testProject/testClass.java
@@ -3,5 +3,7 @@ package testProject;
 public class testClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
+		System.out.println("This is the other branch and has differences");
+		System.out.println("This is a tester branch update");
 	}
 }
