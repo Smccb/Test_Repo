de212238d0dce9ed75794c7107a5c70bd346a33d
Smccb <sarahmmccb@gmail.com>
2022-12-04 16:29:52 +0000

    commit to other branch

--- testProject/src/testProject/testClass.java
+++ testProject/src/testProject/testClass.java
@@ -3,5 +3,6 @@ package testProject;
 public class testClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
+		System.out.println("This is the other branch and has differences");
 	}
 }
