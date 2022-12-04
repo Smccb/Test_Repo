61788ff76e1cf9f79bb405203f270b14a10628bc
Smccb <sarahmmccb@gmail.com>
2022-12-04 16:25:26 +0000

    push to new branch

--- testProject/src/testProject/testClass.java
+++ testProject/src/testProject/testClass.java
@@ -3,6 +3,10 @@ package testProject;
 public class testClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
+<<<<<<< HEAD
 		System.out.println("This is the other branch and has differences");
+=======
+		System.out.println("This is a tester branch update");
+>>>>>>> ea74934 (push to new branch)
 	}
 }
